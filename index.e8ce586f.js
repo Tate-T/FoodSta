daba317f489301194f6114aee73cb9f1bb59816a
Dior <dianakozachenko20081@gmail.com>
2023-06-23 21:06:55 +0300

    Updates

--- index.e8ce586f.js
+++
@@ -1,2 +0,0 @@
-(()=>{let e={openModalBtn:document.querySelector("[data-open-form]"),closeModalBtn:document.querySelector("[data-close-form]"),backdrop:document.querySelector("[data-backdrop]")};function o(){e.backdrop.classList.toggle("is-hidden-form"),"hidden"==document.body.style.overflow?document.body.style.overflow="visible":document.body.style.overflow="hidden"}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o),e.backdrop.addEventListener("click",function(){console.log("Це клік в бекдроп")})})();
-//# sourceMappingURL=index.e8ce586f.js.map
